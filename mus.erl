f96586615c937d577073d64b6af75e1c56d297f4
sanzor <adriansan_93@yahoo.com>
2020-01-02 22:19:28 +0200

    supervisor example

--- mus.erl
+++ mus.erl
@@ -5,14 +5,51 @@
 -define(DELAY,750).
 -record(state,{
     name,
-    skill
+    skill,
+    role
 }).
 
+init([Role,Skill])->
+    process_flag(trap_exit,true),
+    random:seed(now()),
+    TimeToPlay=random:uniform(3000),
+    Name=pick_name(),
+    StrRole=atom_to_list(Role),
+    io:format("Musician ~s, playing  the ~s entered the room ",[Name,StrRole]),
+    {ok,#state{name=Name,role=StrRole,skill=Skill},TimeToPlay}.
 
 handle_call(Message,From,State)->
-    {reply,Message,State}.
+    {stop,normal,ok,S}.
+handle_call(_,_,S)->
+    {noreply,S,?DELAY}.
 
-handle_cast(Message,State=#state{name=N,skill=good}}->
+handle_cast(Message,State}->
     {noreply,State,?DELAY}.
+handle_info(Message,State=#state{name=N,skill=good})->
+    io:format("~s produced good sound",[N]),
+    {noreply,S,?DELAY};
 handle_info(Message,State=#state{name=N,skill=bad})->
-    {stop,}
+    case random:uniform(5) of
+        1->
+            io:format("~s played a false note",[N]),
+            {stop,bad_note,S};
+        _ ->
+            io:format("~s produced good sound",[N]),
+            {noreply,S,?DELAY}
+   end;
+handle_info(Message,State)->
+    {noreply,S,?DELAY}.
+
+
+pick_name() ->
+    lists:nth(random:uniform(10), firstnames())
+    ++ " " ++
+    lists:nth(random:uniform(10), lastnames()).
+         
+firstnames() ->
+    ["Valerie", "Arnold", "Carlos", "Dorothy", "Keesha",
+    "Phoebe", "Ralphie", "Tim", "Wanda", "Janet"].
+         
+lastnames() ->
+    ["Frizzle", "Perlstein", "Ramon", "Ann", "Franklin",
+    "Terese", "Tennelli", "Jamal", "Li", "Perlstein"].
